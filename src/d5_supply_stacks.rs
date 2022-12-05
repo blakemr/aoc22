f4330452c7ffd0053c308a9c3ec6fd3c65800452
Matthew <matthewblake0625@gmail.com>
2022-12-05 12:11:22 -0500

    finished day 4

---
+++ src/d5_supply_stacks.rs
@@ -0,0 +1,152 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+const MOVES: &str = "src\\d5_supply_stacks_moves.txt";
+const START: &str = "src\\d5_supply_stacks_start.csv";
+
+#[derive(Debug)]
+struct MoveOrder {
+    amount: usize,
+    start: usize,
+    end: usize,
+}
+
+impl MoveOrder {
+    fn move_crate(self, crates: &mut [Vec<char>]) {
+        for _ in 0..self.amount {
+            let Some(c) = crates[self.start].pop() else {panic!("No crate to move!")};
+            crates[self.end].push(c);
+        }
+    }
+
+    fn move_multple_crates(self, crates: &mut [Vec<char>]) {
+        let mut c = crates[self.start].split_off(crates[self.start].len() - self.amount);
+        crates[self.end].append(&mut c);
+    }
+}
+
+pub fn find_top_crates() -> String {
+    let moves = import_moves();
+    let mut crates = import_start();
+
+    for order in moves {
+        order.move_crate(&mut crates);
+    }
+
+    let mut top_crates = String::new();
+    for mut stack in crates {
+        let Some(top) = stack.pop() else {panic!("Empty stack!")};
+        top_crates.push(top);
+    }
+
+    top_crates
+}
+
+pub fn find_new_top_crates() -> String {
+    let moves = import_moves();
+    let mut crates = import_start();
+
+    for order in moves {
+        order.move_multple_crates(&mut crates);
+    }
+
+    let mut top_crates = String::new();
+    for mut stack in crates {
+        let Some(top) = stack.pop() else {panic!("Empty stack!")};
+        top_crates.push(top);
+    }
+
+    top_crates
+}
+
+fn import_moves() -> Vec<MoveOrder> {
+    let file = File::open(MOVES).expect("Error opening file.");
+    let reader = BufReader::new(file);
+
+    let move_sep = " from ";
+    let position_sep = " to ";
+
+    let mut orders = Vec::<MoveOrder>::new();
+    for line in reader.lines() {
+        let lin = line.expect("Error reading line.");
+        let mut move_and_position = lin.split(move_sep);
+
+        let Some(mov) = move_and_position.next() else {panic!("line split has no next element (1/2)!")};
+        let Some(move_number) = mov.strip_prefix("move ") else {panic!("Move pattern did not match!")};
+        let amount: usize = move_number.parse().expect("Error parsing move number.");
+
+        let Some(pos) = move_and_position.next() else {panic!("line split has no next element (2/2)!")};
+        let mut start_and_end = pos
+            .split(position_sep)
+            .map(|n| n.parse::<usize>().expect("Unable to parse usize."));
+
+        let Some(mut start) = start_and_end.next() else {panic!("start and end split has no next element (1/2)")};
+        let Some(mut end) = start_and_end.next() else {panic!("start and end split has no next element (2/2)")};
+
+        start -= 1;
+        end -= 1;
+
+        orders.push(MoveOrder { amount, start, end });
+    }
+
+    orders
+}
+
+fn import_start() -> Vec<Vec<char>> {
+    let file = File::open(START).expect("Error opening file.");
+    let reader = BufReader::new(file);
+
+    let mut start_position = Vec::<Vec<char>>::new();
+    for line in reader.lines() {
+        let crates: Vec<char> = line
+            .expect("unable to read line.")
+            .replace(',', "")
+            .chars()
+            .collect();
+
+        start_position.push(crates);
+    }
+
+    start_position
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_new_top_crates, find_top_crates, import_moves, import_start, MoveOrder};
+
+    #[test]
+    fn check_moves_import() {
+        println!("{:?}", import_moves());
+    }
+
+    #[test]
+    fn check_start_import() {
+        println!("{:?}", import_start());
+    }
+
+    #[test]
+    fn test_move_crate() {
+        let mut start = vec![vec!['a', 'b', 'c'], vec!['x', 'y', 'z']];
+        let order = MoveOrder {
+            amount: 2,
+            start: 1 - 1,
+            end: 2 - 1,
+        };
+
+        order.move_crate(&mut start);
+
+        assert_eq!(start[1], vec!['x', 'y', 'z', 'c', 'b']);
+    }
+
+    #[test]
+    fn check_top_crates() {
+        println!("{}", find_top_crates());
+    }
+
+    #[test]
+    fn check_new_top_crates() {
+        println!("{}", find_new_top_crates());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod d1_calorie_counting;
 pub mod d2_rock_paper_scissors;
 pub mod d3_rucksack_reorganization;
 pub mod d4_camp_cleanup;
+pub mod d5_supply_stacks;
